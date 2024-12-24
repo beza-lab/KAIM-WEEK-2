28a6b9e556ad4a0b7bc54c3bab893865503a6175
Bezawit Girma <gahebs@gmail.com>
2024-12-24 19:24:32 +0300

    update dashboard file

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
